e3441fb12a3ca042b9fabe158e654820027cceb9
Andrtisar <andrzey.sheremet@gmail.com>
2021-02-10 18:02:14 +0100

    lesson 13

--- lesson12/demo.js
+++ lesson12/demo.js
@@ -1,1 +1,13 @@
+console.log("Hello World!");
 
+let x;
+x = 1;
+
+let y = 2;
+console.log(x==y);
+
+let z = x + y;
+console.log(z);
+z += " but as a string";
+console.log(z);
+console.log(1.3 + 2.7);
