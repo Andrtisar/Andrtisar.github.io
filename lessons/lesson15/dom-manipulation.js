e65ca88af6600872cbbd12c87c71b6e2d31ae5c8
Andrtisar <andrzey.sheremet@gmail.com>
2021-02-25 18:28:43 +0100

    Lesson 15

---
+++ lessons/lesson15/dom-manipulation.js
@@ -0,0 +1,44 @@
+let gallery = document.getElementById("cats-gallery");
+
+for(let i=1; i<=4; i++) {
+    let img = document.createElement("img");
+    img.src = "img/image" + i + ".jpg";
+
+    gallery.appendChild(img);
+}
+
+let number = Math.floor(Math.random() * 10) + 1;
+
+for (let i=1; i<=number; i++) {
+     let img = document.createElement("img");
+     img.src = "https://loremflickr.com/350/350/cats?random="+i;
+
+     gallery.appendChild(img);
+}
+
+// let count = gallery.childElementCount;
+let count = gallery.children.length;
+let totalElements = document.getElementById("total-cats");
+totalElements.innerText = count;
+
+let firstImage = document.querySelector(".gallery img:first-child");
+
+// firstImage.style.borderStyle = "solid";
+// firstImage.style.borderWidth = "0.25rem";
+// firstImage.style.borderColor = "green";
+
+firstImage.classList.add("red-border");
+
+let headerElement = document.querySelector("h1");
+headerElement.parentElement.removeChild(headerElement);
+
+let footerElement = document.querySelector(".footer");
+gallery.appendChild(footerElement);
+
+
+function handleClick() {
+    console.log("Hi");
+}
+
+let newButton = document.querySelector(".new-button");
+newButton.onclick = handleClick;
